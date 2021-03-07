50b6ae97aae36554880ded89d7a35262196a85b8
Ale Farias <mail.alefarias@gmail.com>
2021-03-06 23:06:37 -0300

    Adds poetry as a new dependecy manager

---
+++ {{ cookiecutter.project_name }}/scripts/code_quality.py
@@ -0,0 +1,37 @@
+import os
+
+
+def check_import_order():
+    os.system("isort --check ./{{ cookiecutter.project_name }}/ --gitignore --dont-follow-links --verbose")
+
+
+def check_code_formatting():
+    os.system("black --check ./{{ cookiecutter.project_name }}/ --exclude __init__.py --verbose")
+
+
+def sort_import_order():
+    os.system("isort --check ./{{ cookiecutter.project_name }}/ --gitignore --dont-follow-links --verbose")
+
+
+def do_code_formatting():
+    os.system("black --check ./{{ cookiecutter.project_name }}/ --exclude __init__.py --verbose")
+
+
+def linter():
+    os.system("pylama ./{{ cookiecutter.project_name }}/ ./tests/")
+
+
+def run_tests():
+    os.system("pytest ./tests/ --verbose --color=yes --code-highlight=yes")
+
+
+def check_code_quality():
+    check_import_order()
+    check_code_formatting()
+    linter()
+    run_tests()
+
+
+def code_formatting():
+    sort_import_order()
+    do_code_formatting()
---
+++ {{ cookiecutter.project_name }}/tests/test_dummy.py
@@ -0,0 +1,7 @@
+import pytest
+
+
+class TestDummy:
+
+    def test_simple_assertion(self):
+        assert 2 == 1+1
